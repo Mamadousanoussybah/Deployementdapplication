ec9e3ae74267c86044d08ee9d21286a100504a59
mamadousanoussybah01 <mamadousanoussybah01@gmail.com>
2026-01-16 22:20:28 -0500

    Initial commit

---
+++ PROJET1/Pro/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices.JavaScript;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("je viens d'arriver en guinée conakry");
+Console.WriteLine("dou viens tu ?");
+Console.WriteLine("je viens d'arriver en guinée conakry");
+Console.WriteLine("je viens d'arriver en guinée conakry");
+
+Console.WriteLine("je viens d'arriver en guinée conakry");
+Console.WriteLine("finalisation de mon projet ");
