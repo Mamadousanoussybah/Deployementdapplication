ad3fae2885c768c841338199ba790fd70606396f
mamadousanoussybah01 <mamadousanoussybah01@gmail.com>
2026-01-16 22:09:45 -0500

    Suppression du faux sous-module

---
+++ PROJET2/PROJET2/Infra2/WebApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
+string KeyVaultURI = Environment.GetEnvironmentVariable("KeyVaultUri");
+Console.WriteLine("KeyVault URI : " + KeyVaultURI);
+
+builder.Configuration.AddAzureKeyVault(
+    new Uri(KeyVaultURI),
+    new DefaultAzureCredential());
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
